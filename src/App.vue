4542197634435ac821e7e4dfcaaec72a08257627
TGS <tgs@tgs.blue>
2017-07-31 07:34:09 +0800

    Init LinkProgress Component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <LinkProgress></LinkProgress>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import LinkProgress from './components/LinkProgress'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    LinkProgress
   }
 }
 </script>
---
+++ src/components/LinkProgress.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        123
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LinkProgress',
+    data() {
+        return {
+            test:`That's OK`
+        }
+    }
+}
+</script>
+
+<style scoped>
+a {
+    color: #42b983;
+}
+</style>
