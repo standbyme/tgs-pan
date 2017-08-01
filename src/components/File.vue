dc69e40a4ce9471c7711518e751c51b4d4c3a52d
TGS <tgs@tgs.blue>
2017-08-01 22:18:18 +0800

    Make File Component break-word when filename is too long

--- src/components/File.vue
+++ src/components/File.vue
@@ -1,26 +1,30 @@
 <template>
-  <div>
-    <a :href="url" target="_blank">{{filename}}</a>
-  </div>
+    <div class="container">
+        <a :href="url" target="_blank">{{filename}}</a>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'file',
-  props: ['fileData'],
-  data: function () {
-    const filename = this.fileData.name
-    const url = this.fileData.url
-    return {
-      filename,
-      url
+    name: 'file',
+    props: ['fileData'],
+    data: function () {
+        const filename = this.fileData.name
+        const url = this.fileData.url
+        return {
+            filename,
+            url
+        }
     }
-  }
 }
 </script>
 
 <style scoped>
+.container {
+    word-break: break-word;
+}
+
 a {
-  font-size:200%;
+    font-size: 200%;
 }
 </style>
